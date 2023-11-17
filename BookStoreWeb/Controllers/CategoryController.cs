bfa749a2373eded1ef2c6ec2521582e83959e1cc
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-17 14:29:06 +0700

    :hammer: create Category view

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -16,5 +16,11 @@ namespace BookStoreWeb.Controllers
             List<Category> objCategoryList = _db.Categories.ToList();
             return View(objCategoryList);
         }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
     }
 }
