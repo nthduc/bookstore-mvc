32a516f1e4211cc525b862d414b8292b89121961
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-17 21:33:44 +0700

    :hammer: Toastr Nofitication Custom

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -32,6 +32,7 @@ namespace BookStoreWeb.Controllers
             {
                 _db.Categories.Add(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index", "Category"); // View,Controller
             }
             return View();
@@ -58,6 +59,7 @@ namespace BookStoreWeb.Controllers
             {
                 _db.Categories.Update(obj);
                 _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -88,6 +90,7 @@ namespace BookStoreWeb.Controllers
             }
             _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
 
         }
