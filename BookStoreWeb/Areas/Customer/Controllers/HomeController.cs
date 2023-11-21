a59b854d2fd2076872d4e8c81eb8e7e487bebcbc
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-21 20:49:40 +0700

    :hammer: hiển thị sản phẩm trang Home

--- BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using BookStore.DataAccess.Repository.IRepository;
 using BookStore.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,15 +9,18 @@ namespace BookStoreWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
