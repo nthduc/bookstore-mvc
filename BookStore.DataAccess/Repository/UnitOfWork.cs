7a169e331e6101aa228b8de1b51c495c060c32fc
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-25 12:07:41 +0700

    :hammer: Giỏ hàng

--- BookStore.DataAccess/Repository/UnitOfWork.cs
+++ BookStore.DataAccess/Repository/UnitOfWork.cs
@@ -20,6 +20,9 @@ namespace BookStore.DataAccess.Repository
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
         public ICompanyRepository Company { get; private set; }
+        public IShoppingCartRepository ShoppingCart { get; private set; }
+        public IApplicationUserRepository ApplicationUser { get; private set; }
+
 
         public UnitOfWork(ApplicationDbContext db)
         {
@@ -27,6 +30,8 @@ namespace BookStore.DataAccess.Repository
             Category = new CategoryRepository(_db);
             Product = new ProductRepository(_db);
             Company = new CompanyRepository(_db);
+            ShoppingCart = new ShoppingCartRepository(_db);
+            ApplicationUser = new ApplicationUserRepository(_db);
         }
 
         public void Save()
---
+++ BookStore.Models/ShoppingCart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+namespace BookStore.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+        [Range(1, 1000, ErrorMessage = "Vui lòng nhập giá trị từ 1 đến 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
