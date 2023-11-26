878b224c0d4c79e64ef50680b3fa2eb402700436
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-26 20:51:28 +0700

    :hammer: Status Order

--- BookStore.Models/ViewModels/ShoppingCartVM.cs
+++ BookStore.Models/ViewModels/ShoppingCartVM.cs
@@ -10,6 +10,6 @@ namespace BookStore.Models.ViewModels
     {
         public IEnumerable<ShoppingCart> ShoppingCartList { get; set; }
 
-        public double OrderTotal { get; set; }
+        public OrderHeader OrderHeader { get; set; }
     }
 }
