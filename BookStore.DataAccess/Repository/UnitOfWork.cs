ef6101482e19fabb45782abaab70ae1476e2c2e3
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-26 20:44:33 +0700

    :hammer: Add Order Repository

--- BookStore.DataAccess/Repository/UnitOfWork.cs
+++ BookStore.DataAccess/Repository/UnitOfWork.cs
@@ -22,6 +22,8 @@ namespace BookStore.DataAccess.Repository
         public ICompanyRepository Company { get; private set; }
         public IShoppingCartRepository ShoppingCart { get; private set; }
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
 
         public UnitOfWork(ApplicationDbContext db)
@@ -32,6 +34,8 @@ namespace BookStore.DataAccess.Repository
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
