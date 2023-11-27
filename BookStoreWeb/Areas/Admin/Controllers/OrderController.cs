74e3a996db38c24db8e52570ca23ceacf66cf1af
nthduc <nguyenthaiduc0212@gmail.com>
2023-11-27 22:39:10 +0700

    :hammer: Order

---
+++ BookStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreWeb.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return Json(new { data = objOrderHeaders });
+        }
+
+
+        #endregion
+    }
+}
