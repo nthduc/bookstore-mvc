20c398123be2651070657bffbed0d9eb0221c1f5
nthduc <nguyenthaiduc0212@gmail.com>
2024-01-13 18:02:18 +0700

    :hammer: Chi tiết đơn hàng và Sản phẩm API

---
+++ BookStoreAPI/Controllers/OrderDetailController.cs
@@ -0,0 +1,77 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderDetailController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        // GET all order details
+        public IActionResult GetOrderDetails()
+        {
+            var orderDetails = _unitOfWork.OrderDetail.GetAll(includeProperties: "OrderHeader,Product");
+            return Ok(orderDetails);
+        }
+
+
+        [HttpGet("{id}")]
+        // Get an order detail by Id
+        public IActionResult GetOrderDetail(int id)
+        {
+            var orderDetail = _unitOfWork.OrderDetail.Get(o => o.Id == id, includeProperties: "OrderHeader,Product");
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+            return Ok(orderDetail);
+        }
+
+        [HttpPost]
+        // Create a new order detail
+        public IActionResult CreateOrderDetail(OrderDetail orderDetail)
+        {
+            _unitOfWork.OrderDetail.Add(orderDetail);
+            _unitOfWork.Save();
+            return CreatedAtAction(nameof(GetOrderDetail), new { id = orderDetail.Id }, orderDetail);
+        }
+
+        [HttpPut("{id}")]
+        // Update an order detail
+        public IActionResult UpdateOrderDetail(int id, OrderDetail orderDetail)
+        {
+            if (id != orderDetail.Id)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.OrderDetail.Update(orderDetail);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        // Delete an order detail
+        public IActionResult DeleteOrderDetail(int id)
+        {
+            var orderDetail = _unitOfWork.OrderDetail.Get(o => o.Id == id);
+
+            if (orderDetail == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.OrderDetail.Remove(orderDetail);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+    }
+}
---
+++ BookStoreAPI/Controllers/ProductController.cs
@@ -0,0 +1,76 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        // GET all products
+        public IActionResult GetProducts()
+        {
+            var products = _unitOfWork.Product.GetAll();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        // Get a product by Id
+        public IActionResult GetProduct(int id)
+        {
+            var product = _unitOfWork.Product.Get(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        // Create a new product
+        public IActionResult CreateProduct(Product product)
+        {
+            _unitOfWork.Product.Add(product);
+            _unitOfWork.Save();
+            return CreatedAtAction(nameof(GetProduct), new { id = product.Id }, product);
+        }
+
+        [HttpPut("{id}")]
+        // Update a product
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.Product.Update(product);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        // Delete a product
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _unitOfWork.Product.Get(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+    }
+}
