6e67e3b57128e03d0675be3e004730f78f1409f5
nthduc <nguyenthaiduc0212@gmail.com>
2024-01-13 18:22:11 +0700

    :hammer: Thông tin và Giỏ hàng API

---
+++ BookStoreAPI/Controllers/CompanyController.cs
@@ -0,0 +1,76 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        // GET all companies
+        public IActionResult GetCompanies()
+        {
+            var companies = _unitOfWork.Company.GetAll();
+            return Ok(companies);
+        }
+
+        [HttpGet("{id}")]
+        // Get a company by Id
+        public IActionResult GetCompany(int id)
+        {
+            var company = _unitOfWork.Company.Get(c => c.Id == id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+
+        [HttpPost]
+        // Create a new company
+        public IActionResult CreateCompany(Company company)
+        {
+            _unitOfWork.Company.Add(company);
+            _unitOfWork.Save();
+            return CreatedAtAction(nameof(GetCompany), new { id = company.Id }, company);
+        }
+
+        [HttpPut("{id}")]
+        // Update a company
+        public IActionResult UpdateCompany(int id, Company company)
+        {
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.Company.Update(company);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        // Delete a company
+        public IActionResult DeleteCompany(int id)
+        {
+            var company = _unitOfWork.Company.Get(c => c.Id == id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Company.Remove(company);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+    }
+}
---
+++ BookStoreAPI/Controllers/ShoppingCartController.cs
@@ -0,0 +1,76 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ShoppingCartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        // GET all shopping carts (giỏ hàng)
+        public IActionResult GetShoppingCarts()
+        {
+            var shoppingCarts = _unitOfWork.ShoppingCart.GetAll();
+            return Ok(shoppingCarts);
+        }
+
+        [HttpGet("{id}")]
+        // Get a shopping cart by Id
+        public IActionResult GetShoppingCart(int id)
+        {
+            var shoppingCart = _unitOfWork.ShoppingCart.Get(s => s.Id == id);
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            return Ok(shoppingCart);
+        }
+
+        [HttpPost]
+        // Create a new shopping cart
+        public IActionResult CreateShoppingCart(ShoppingCart shoppingCart)
+        {
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+            return CreatedAtAction(nameof(GetShoppingCart), new { id = shoppingCart.Id }, shoppingCart);
+        }
+
+        [HttpPut("{id}")]
+        // Update a shopping cart
+        public IActionResult UpdateShoppingCart(int id, ShoppingCart shoppingCart)
+        {
+            if (id != shoppingCart.Id)
+            {
+                return BadRequest();
+            }
+            _unitOfWork.ShoppingCart.Update(shoppingCart);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        // Delete a shopping cart
+        public IActionResult DeleteShoppingCart(int id)
+        {
+            var shoppingCart = _unitOfWork.ShoppingCart.Get(s => s.Id == id);
+
+            if (shoppingCart == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.ShoppingCart.Remove(shoppingCart);
+            _unitOfWork.Save();
+            return NoContent();
+        }
+    }
+}
